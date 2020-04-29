20b0c7694c77167885e6e61b0da5f0ffba27ad50
Laurence Rowe <laurence@lrowe.co.uk>
2020-04-28 20:30:40 -0700

    Add timing

--- src/bin/convert.rs
+++ src/bin/convert.rs
@@ -2,6 +2,7 @@ extern crate flexbuffers;
 extern crate serde_json;
 use std::env;
 use std::io::{self, Read, Write};
+use std::time::Instant;
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -10,21 +11,33 @@ fn main() -> io::Result<()> {
             let format_in = &args[1];
             let format_out = &args[2];
             let mut buf_in = Vec::new();
+            let begin = Instant::now();
             io::stdin().lock().read_to_end(&mut buf_in)?;
 
+            let begin_de = Instant::now();
             let value: serde_json::Value = match &format_in[..] {
                 "json" => serde_json::from_slice(&buf_in)?,
                 "flexbuffer" => flexbuffers::from_slice(&buf_in).unwrap(),
                 _ => panic!("invalid arg format_in"),
             };
 
+            let begin_ser = Instant::now();
             let buf_out = match &format_out[..] {
                 "json" => serde_json::to_vec(&value)?,
                 "flexbuffer" => flexbuffers::to_vec(&value).unwrap(),
                 _ => panic!("invalid arg format_out"),
             };
+            let end_ser = Instant::now();
 
             io::stdout().lock().write_all(&buf_out)?;
+            let end = Instant::now();
+
+            eprintln!(
+                "total:{:?} de:{:?} output:{:?}",
+                end.duration_since(begin),
+                begin_ser.duration_since(begin_de),
+                end_ser.duration_since(begin_ser)
+            );
             Ok(())
         }
         _ => {
