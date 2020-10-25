b007486b403b5459961acfaeba77e005d9342912
Laurence Rowe <laurence@lrowe.co.uk>
2020-10-24 19:41:22 -0700

    Fix convert to use serde_transcode.

--- src/bin/convert.rs
+++ src/bin/convert.rs
@@ -1,53 +1,98 @@
+extern crate clap;
 extern crate flexbuffers;
 extern crate serde_json;
-use std::env;
+extern crate serde_transcode;
+use std::error::Error;
+use std::fs::File;
 use std::io::{self, Read, Write};
-use std::time::Instant;
+use std::path::Path;
 
-fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    match args.len() {
-        3 => {
-            let format_in = &args[1];
-            let format_out = &args[2];
-            let mut buf_in = Vec::new();
-            let begin = Instant::now();
-            io::stdin().lock().read_to_end(&mut buf_in)?;
+fn main() -> Result<(), Box<dyn Error>> {
+    let matches = clap::App::new("Convert")
+        .arg_from_usage("--in [FILE] 'input file'")
+        .arg_from_usage("--out [FILE] 'output file'")
+        .arg(
+            clap::Arg::from_usage("--format-in [TYPE] 'input type (json|bin)'")
+                .possible_values(&["json", "bin"]),
+        )
+        .arg(
+            clap::Arg::from_usage("--format-out [TYPE] 'output type (json|bin)'")
+                .possible_values(&["json", "bin"]),
+        )
+        .get_matches();
 
-            let begin_de = Instant::now();
-            let builder: flexbuffers::Builder = match &format_in[..] {
-                "json" => serde_json::from_slice(&buf_in)?,
-                "flexbuffer" => flexbuffers::from_slice(&buf_in).unwrap(),
-                _ => panic!("invalid arg format_in"),
-            };
+    let format_in = match matches.value_of("format-in") {
+        Some(format) => format,
+        _ => match matches.value_of("in") {
+            Some(path) => match Path::new(&path).extension().and_then(|ext| ext.to_str()) {
+                Some("json") => "json",
+                Some("bin") => "bin",
+                _ => panic!("must specify format-in"),
+            },
+            _ => panic!("must specify format-in"),
+        },
+    };
 
-            let begin_ser = Instant::now();
-            let buffer = builder.view();
-            let reader = flexbuffers::Reader::get_root(buffer).unwrap();
-            let buf_out = match &format_out[..] {
-                "json" => serde_json::to_vec(&reader)?,
-                "flexbuffer" => flexbuffers::to_vec(&reader).unwrap(),
-                _ => panic!("invalid arg format_out"),
-            };
-            let end_ser = Instant::now();
+    let format_out = match matches.value_of("format-out") {
+        Some(format) => format,
+        _ => match matches.value_of("out") {
+            Some(path) => match Path::new(&path).extension().and_then(|ext| ext.to_str()) {
+                Some("json") => "json",
+                Some("bin") => "bin",
+                _ => panic!("must specify format-out"),
+            },
+            _ => panic!("must specify format-out"),
+        },
+    };
 
-            io::stdout().lock().write_all(&buf_out)?;
-            let end = Instant::now();
+    let mut reader: Box<dyn Read> = match matches.value_of("in") {
+        Some(path) if path != "-" => Box::new(File::open(path)?),
+        _ => Box::new(io::stdin()),
+    };
+    let mut buf_in = Vec::new();
+    reader.read_to_end(&mut buf_in)?;
 
-            eprintln!(
-                "total:{:?} de:{:?} output:{:?}",
-                end.duration_since(begin),
-                begin_ser.duration_since(begin_de),
-                end_ser.duration_since(begin_ser)
-            );
-            Ok(())
+    let mut writer: Box<dyn Write> = match matches.value_of("out") {
+        Some(path) if path != "-" => Box::new(File::create(path)?),
+        _ => Box::new(io::stdout()),
+    };
+
+    // Each branch must be separate because of type specialization.
+    // Cannot use Box<dyn Serializer> because serde::Serializer lacks Sized.
+    match format_out {
+        "json" => {
+            let mut serializer = serde_json::Serializer::new(&mut writer);
+            match format_in {
+                "json" => {
+                    let mut deserializer = serde_json::Deserializer::from_slice(&buf_in);
+                    serde_transcode::transcode(&mut deserializer, &mut serializer)?;
+                    deserializer.end()?;
+                }
+                "bin" => {
+                    let deserializer = flexbuffers::Reader::get_root(&buf_in)?;
+                    serde_transcode::transcode(deserializer, &mut serializer)?;
+                }
+                _ => panic!("unreachable"),
+            }
+            writer.write_all(b"\n")?;
         }
-        _ => {
-            eprintln!(
-                "usage: {} <format_in> <format_out>\nformats: json, flexbuffer",
-                args[0]
-            );
-            panic!("invalid args");
+        "bin" => {
+            let mut serializer = flexbuffers::FlexbufferSerializer::new();
+            match format_in {
+                "json" => {
+                    let mut deserializer = serde_json::Deserializer::from_slice(&buf_in);
+                    serde_transcode::transcode(&mut deserializer, &mut serializer)?;
+                    deserializer.end()?;
+                }
+                "bin" => {
+                    let deserializer = flexbuffers::Reader::get_root(&buf_in)?;
+                    serde_transcode::transcode(deserializer, &mut serializer)?;
+                }
+                _ => panic!("unreachable"),
+            }
+            writer.write_all(serializer.view())?;
         }
+        _ => panic!("unreachable"),
     }
+    Ok(())
 }
